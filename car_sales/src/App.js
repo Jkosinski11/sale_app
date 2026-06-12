fa904af3539a3c71e7101cfa858d6e0430dfbebf
Jkosinski11 <kosinski.jacob11@gmail.com>
2026-06-12 13:23:40 -0400

    add google tag

--- car_sales/src/App.js
+++ car_sales/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 
 function App() {
   return (
+        
     <div className="App">
+    
       <img src= '/ford_bronco.jpeg' alt='Ford Bronco'/>
       <h1 id = 'car'>Ford Bronco</h1>
       <span id= 'car-cost'>$20500</span>
