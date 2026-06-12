2af3910a5239559922e4baf6ce1e4da58fbb88b4
Jkosinski11 <kosinski.jacob11@gmail.com>
2026-06-12 12:46:26 -0400

    initial commit

---
+++ car_sales/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src= '/ford_bronco.jpeg' alt='Ford Bronco'/>
+      <h1 id = 'car'>Ford Bronco</h1>
+      <span id= 'car-cost'>$20500</span>
+    </div>
+  );
+}
+
+export default App;
